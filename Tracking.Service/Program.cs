2a26246c1cb4065fc9a2184b07173325375604c9
M.B <mb0583219718@gmail.com>
2020-07-09 15:06:40 +0300

    handlers nsb

---
+++ Tracking.Service/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Tracking.Service
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
